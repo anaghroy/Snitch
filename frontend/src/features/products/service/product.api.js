943ca48e3f4f2ef4b38390cd28406f22f4a49d5d
AnaghRoy <royanagh1999@gmail.com>
2026-04-21 22:37:58 +0530

    Add on features

--- frontend/src/features/products/service/product.api.js
+++ frontend/src/features/products/service/product.api.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
 const productApiInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/products",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+  baseURL: "/api/products",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export async function createProduct(formData) {
     const response = await productApiInstance.post("/create", formData, {
